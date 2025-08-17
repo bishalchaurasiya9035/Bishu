ad9b303fb2cfd43214500a08ce285f88cd31572d
bishalchaurasiya9035 <bishalchaurasiya9035@gmail.com>
2025-08-17 21:06:18 +0530

    Added more API's

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,12 +1,33 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.Users;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class HelloController {
-    @GetMapping("/Hello")
+
+    private List<Users> usersList = new ArrayList<>();
+
+    @GetMapping("/hello")
     public String SayHello(){
         return "Hello, Spring Boot";
     }
+
+    @PostMapping("/user")
+    public String createUser(@RequestBody Users user){
+        usersList.add(user);
+        return "User added: " + user.getName();
+    }
+
+    @GetMapping("userslist")
+    public List<Users> getUsersList(){
+    return usersList;
+    }
+
 }
